64e42e66ef0d729fdbb59a1eddd37e6deacda634
Alexander McGilvray <alex@levelism.com>
2015-03-22 10:12:30 -0700

    added joystick button state and info structs

--- FighterInterface/JoystickController.h
+++ FighterInterface/JoystickController.h
@@ -7,6 +7,8 @@ namespace Input
 {
 	namespace Joystick
 	{ 
+		typedef unsigned char JoystickButton;
+
 		class JoystickController : public IController
 		{
 		public:
@@ -16,6 +18,27 @@ namespace Input
 			void HardwareID(int val);
 		private:
 			int hardwareID;
+
+			enum JoystickButtonState
+			{
+				BUTTON_NOT_FOUND = -2,
+				BUTTON_NOT_MAPPED = -1, 
+				BUTTON_DOWN = 1, //same as what glfw returns
+				BUTTON_UP = 0 //same as what glfw returns
+			};
+
+			struct JoystickButtonInfo
+			{
+				JoystickButtonInfo(){}
+				JoystickButtonInfo(JoystickButtonState state, JoystickButton button):
+					state(state),
+					button(button)
+				{ } 
+				JoystickButton button; //physical button
+				JoystickButtonState state; //state of the button
+			}; 
 		};  
+
+		
 	} 
 }
