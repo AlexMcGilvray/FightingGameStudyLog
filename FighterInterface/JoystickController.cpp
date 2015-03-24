593a1c0be64a9d3516aa0ead4f4d19c26d927216
Alexander McGilvray <alex@levelism.com>
2015-03-23 21:12:05 -0700

    final joystick commit for the night

--- FighterInterface/JoystickController.cpp
+++ FighterInterface/JoystickController.cpp
@@ -14,15 +14,29 @@ Input::InputInfo Input::Joystick::JoystickController::Poll()
 	auto glfwButtons = glfwGetJoystickButtons	(hardwareID, &buttonCount);
 	//take the smaller of the two button arrays so we dont go out of bounds
 	int buttonIterations = JOYSTICK_HARDWARE_BUTTON_MAX < buttonCount ? JOYSTICK_HARDWARE_BUTTON_MAX : buttonCount;
-	
-
+	buttonIterations = buttonIterations < FINAL_VIRTUAL_KEY_ENUM_VALUE ? buttonIterations : FINAL_VIRTUAL_KEY_ENUM_VALUE;
+	 
 	//process buttons to get virutal key return
 	for (int i = 0; i < buttonIterations; ++i)
 	{ 
 		 //logic here
-	}
-
-
+		if (lastFrameHardwareButtons[i] == GLFW_PRESS && glfwButtons[i] == GLFW_RELEASE)
+		{
+			result.inputMapping[i] = VIRTUAL_KEY_RELEASED;
+		}
+		else if (lastFrameHardwareButtons[i] == GLFW_RELEASE && glfwButtons[i] == GLFW_PRESS)
+		{
+			result.inputMapping[i] = VIRTUAL_KEY_PRESSED;
+		}
+		else if (lastFrameHardwareButtons[i] == GLFW_PRESS && glfwButtons[i] == GLFW_PRESS)
+		{
+			result.inputMapping[i] = VIRTUAL_KEY_DOWN;
+		}
+		else if (lastFrameHardwareButtons[i] == GLFW_PRESS && glfwButtons[i] == GLFW_PRESS)
+		{
+			result.inputMapping[i] = VIRTUAL_KEY_UP;
+		}
+	} 
 	//copy buttons to last frame buttons
 	for (int i = 0; i < buttonIterations; ++i)
 	{
