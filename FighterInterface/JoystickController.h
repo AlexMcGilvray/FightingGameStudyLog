a7dfa597a02cb2a61ca28e68b757b289ad7a9e5a
Alexander McGilvray <alex@levelism.com>
2015-03-23 19:46:15 -0700

    ripped out the guts of the JoystickController interface because it was too bloated due to the fact it was originally an adaptation of the keyboard controller class. The joystick needs to be respected and given its own unique consideration in order to have a simpler interface and better management

--- FighterInterface/JoystickController.h
+++ FighterInterface/JoystickController.h
@@ -2,51 +2,24 @@
 #include "Input.h"
  
 using namespace Input::VirtualInput; 
-
-
-
+ 
 namespace Input
 {
 	namespace Joystick
 	{ 
-
+		const int JOYSTICK_HARDWARE_BUTTON_MAX = 10; //number of button indices we poll, currently an arbitrary value
 		typedef unsigned char JoystickButton;
 		const unsigned char JOYSTICK_BUTTON_UNINITIALIZED = 100;
 
 		class JoystickController : public IController
-		{ 
-		private: //nested private data definitions 
-			enum JoystickButtonState //possible joystick button states
-			{
-				BUTTON_NOT_FOUND = -2,
-				BUTTON_NOT_MAPPED = -1, 
-				BUTTON_DOWN = 1, //same as what glfw returns
-				BUTTON_UP = 0 //same as what glfw returns
-			};
-
-			struct JoystickButtonInfo
-			{
-				JoystickButtonInfo(){}
-				JoystickButtonInfo(JoystickButtonState state, JoystickButton button):
-					state(state),
-					button(button)
-				{ } 
-				JoystickButton button; //physical button
-				JoystickButtonState state; //state of the button
-			};  
+		{  
 		public:
 			virtual InputInfo Poll();
 			virtual void DebugDraw();
 			int HardwareID();
 			void HardwareID(int val);
 		private:
-			int hardwareID;
-			bool ButtonDown(JoystickButton button);
-			bool ButtonPressed(JoystickButton button);
-			bool ButtonReleased(JoystickButton button);
-			JoystickButtonInfo mappingInfo[FINAL_VIRTUAL_KEY_ENUM_VALUE]; 
-
-			
+			int hardwareID;   
 		};  
 
 		//Todo add a joystick mapping system so people can write joystick data files to make custom JoystickButtonInfos
